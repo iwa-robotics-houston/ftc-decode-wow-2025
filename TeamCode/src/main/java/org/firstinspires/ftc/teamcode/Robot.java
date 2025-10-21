0a5f80ba933b54baf26448a41e8d7621bd114273
smichael28 <sophiemichael211@gmail.com>
2025-10-20 21:34:07 -0500

    Commented out some code we don't need for the gobilda robot- Sophie Michael

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -87,7 +87,7 @@ public class Robot {
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // Configures encoders
+        // Configure encoders
         drivetrainSetRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         drivetrainSetRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
          */
