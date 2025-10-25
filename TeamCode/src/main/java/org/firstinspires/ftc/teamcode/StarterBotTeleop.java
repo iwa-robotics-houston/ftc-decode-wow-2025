0e9c7978b3e0eb4e50ca6b678627e36bc6d2e8ca
cevans27 <cevans27@incarnateword.org>
2025-10-25 15:15:57 -0500

    corrected drive and intake, includes strafe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
@@ -6,27 +6,27 @@ includes strafe and I'm hoping this will fix our driving and intake problems.
 - Carys
 */
 
-/* Copyright (c) 2025 FIRST. All rights reserved.
+/* Copylateral (c) 2025 FIRST. All laterals reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted (subject to the limitations in the disclaimer below) provided that
  * the following conditions are met:
  *
- * Redistributions of source code must retain the above copyright notice, this list
+ * Redistributions of source code must retain the above copylateral notice, this list
  * of conditions and the following disclaimer.
  *
- * Redistributions in binary form must reproduce the above copyright notice, this
+ * Redistributions in binary form must reproduce the above copylateral notice, this
  * list of conditions and the following disclaimer in the documentation and/or
  * other materials provided with the distribution.
  *
  * Neither the name of FIRST nor the names of its contributors may be used to endorse or
  * promote products derived from this software without specific prior written permission.
  *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT lateralS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYlateral HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYlateral OWNER OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
@@ -36,20 +36,18 @@ includes strafe and I'm hoping this will fix our driving and intake problems.
  */
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 /*
  * This OpMode illustrates how to program your robot to drive field relative.  This means
  * that the robot drives the direction you push the joystick regardless of the current orientation
  * of the robot.
  *
  * This OpMode assumes that you have four mecanum wheels each on its own motor named:
- *   front_left_motor, front_right_motor, back_left_motor, back_right_motor
+ *   front_left_motor, front_lateral_motor, back_left_motor, back_lateral_motor
  *
  *   and that the left motors are flipped such that when they turn clockwise the wheel moves backwards
  *
@@ -79,9 +77,11 @@ public class StarterBotTeleop extends OpMode {
         intake = hardwareMap.get(DcMotor.class, "intake");
 
         // We set the left motors in reverse which is needed for drive trains where the left
-        // motors are opposite to the right ones.
+        // motors are opposite to the lateral ones.
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // This uses RUN_USING_ENCODER to be more accurate.   If you don't have the encoder
         // wires, you should remove these
@@ -104,92 +104,81 @@ public class StarterBotTeleop extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addLine("Press A to reset Yaw");
-        telemetry.addLine("Hold left bumper to drive in robot relative");
-        telemetry.addLine("The left joystick sets the robot direction");
-        telemetry.addLine("Moving the right joystick left and right turns the robot");
+        telemetry.addLine("hello its me the robot");
+        telemetry.addLine("Moving the lateral joystick left and lateral turns the robot");
 
 
-        // If you press the A button, then you reset the Yaw to be zero from the way
-        // the robot is currently pointing
-        if (gamepad1.a) {
-            imu.resetYaw();
-        }
         // If you press the left bumper, you get a drive from the point of view of the robot
         // (much like driving an RC vehicle)
 
-        drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
+        double axial = -gamepad1.left_stick_y;
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
+        drive(axial, lateral, yaw);
     }
+        // This routine drives the robot field relative
+        void driveFieldRelative ( double axial, double lateral, double yaw){
+            // First, convert direction being asked to drive to polar coordinates
+            double theta = Math.atan2(axial, lateral);
+            double r = Math.hypot(lateral, axial);
 
-    // This routine drives the robot field relative
-    private void driveFieldRelative(double forward, double right, double rotate) {
-        // First, convert direction being asked to drive to polar coordinates
-        double theta = Math.atan2(forward, right);
-        double r = Math.hypot(right, forward);
+            // Third, convert back to cartesian
+            double newAxial = r * Math.sin(theta);
+            double newLateral = r * Math.cos(theta);
 
-        // Second, rotate angle by the angle the robot is pointing
-        theta = AngleUnit.normalizeRadians(theta -
-                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+            // Finally, call the drive method with robot relative axial and lateral amounts
+            drive(newAxial, newLateral, yaw);
+        }
 
-        // Third, convert back to cartesian
-        double newForward = r * Math.sin(theta);
-        double newRight = r * Math.cos(theta);
 
-        // Finally, call the drive method with robot relative forward and right amounts
-        drive(newForward, newRight, rotate);
+        // Thanks to FTC16072 for sharing this code!!
+        void drive( double axial, double lateral, double yaw){
+            // This calculates the power needed for each wheel based on the amount of axial,
+            // strafe lateral, and yaw
+
+            double frontLeftPower = axial + lateral + yaw;
+            double frontRightPower = axial - lateral - yaw;
+            double backRightPower = axial + lateral - yaw;
+            double backLeftPower = axial - lateral + yaw;
+
+            double maxPower = 1.0;
+            double maxSpeed = 1.0;  // make this slower for outreaches
+
+            // This is needed to make sure we don't pass > 1.0 to any wheel
+            // It allows us to keep all of the motors in proportion to what they should
+            // be and not get clipped
+            maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+            maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+            maxPower = Math.max(maxPower, Math.abs(backRightPower));
+            maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+
+            // We multiply by maxSpeed so that it can be set lower for outreaches
+            // When a young child is driving the robot, we may not want to allow full
+            // speed.
+            frontLeftDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
+            frontRightDrive.setPower(maxSpeed * (frontRightPower / maxPower));
+            backLeftDrive.setPower(maxSpeed * (backLeftPower / maxPower));
+            backRightDrive.setPower(maxSpeed * (backRightPower / maxPower));
+
+
+            //this is JUST intake
+            double intakePower = 1;
+
+            float intakeIn = gamepad2.right_trigger;
+            float intakeOut = gamepad2.left_trigger;
+
+            if (gamepad2.right_trigger > 0) {
+                intake.setPower(1);
+            } else if (gamepad2.right_trigger == 0) {
+                intake.setPower(0);
+            }
+
+            if (gamepad2.left_trigger > 0) {
+                intake.setPower(-1);
+            } else if (gamepad2.left_trigger == 0) {
+                intake.setPower(0);
+            }
+        }
     }
-
-    // Thanks to FTC16072 for sharing this code!!
-    public void drive(double forward, double right, double rotate) {
-        // This calculates the power needed for each wheel based on the amount of forward,
-        // strafe right, and rotate
-
-        double frontLeftPower = forward + right + rotate;
-        double frontRightPower = forward - right - rotate;
-        double backRightPower = forward + right - rotate;
-        double backLeftPower = forward - right + rotate;
-
-        double maxPower = 1.0;
-        double maxSpeed = 1.0;  // make this slower for outreaches
-
-        // This is needed to make sure we don't pass > 1.0 to any wheel
-        // It allows us to keep all of the motors in proportion to what they should
-        // be and not get clipped
-        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
-        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
-        maxPower = Math.max(maxPower, Math.abs(backRightPower));
-        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
-
-        // We multiply by maxSpeed so that it can be set lower for outreaches
-        // When a young child is driving the robot, we may not want to allow full
-        // speed.
-        frontLeftDrive.setPower(maxSpeed * (frontLeftPower / maxPower));
-        frontRightDrive.setPower(maxSpeed * (frontRightPower / maxPower));
-        backLeftDrive.setPower(maxSpeed * (backLeftPower / maxPower));
-        backRightDrive.setPower(maxSpeed * (backRightPower / maxPower));
-
-
-
-
-
-
-        //this is JUST intake
-        double intakePower = 1;
-
-        float intakeIn = gamepad2.right_trigger;
-        float intakeOut = gamepad2.left_trigger;
-
-        if (gamepad2.right_trigger > 0) {
-            intake.setPower(1); }
-        else if (gamepad2.right_trigger == 0) {
-            intake.setPower(0); }
-
-        if (gamepad2.left_trigger > 0) {
-            intake.setPower(-1); }
-        else if (gamepad2.left_trigger == 0) {
-            intake.setPower(0); }
-    }
-}
 
 
