ebf3b827aaf058201a08a85fe2de7aded854146d
FatherRememberLouisFromMyTenthBirthday <addythescout@gmail.com>
2025-12-11 16:26:31 -0600

    we don't need this file anymore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-//Just realized WOW last year had a Robot.Java for a reason
-//Some tutorials I wanted to use for more complex mechanics used one
-//So I shall be making a Robot.Java
-// - Addy
-public class Robot {
-}
