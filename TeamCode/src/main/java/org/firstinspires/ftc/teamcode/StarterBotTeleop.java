437982c61f24969b5bcf1aa8f67573b3b6a94186
smichael28 <sophiemichael211@gmail.com>
2026-01-06 17:13:57 -0600

    1/6/26 fixed limelight code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
@@ -90,7 +90,7 @@ public class StarterBotTeleop extends OpMode {
     CRServo launcherRight;
     Servo diverter;
 
-    /*
+/*
    //Blue limelight code AprilTag 20 pipeline 0
     public class aprilTagBlueLimelightTest extends OpMode{
         private Limelight3A limelight;
@@ -103,9 +103,8 @@ public class StarterBotTeleop extends OpMode {
             limelight.start();
 
             imu = hardwareMap.get(IMU.class,"imu");
-            RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                    RevHubOrientationOnRobot.UsbFacingDirection.DOWN);
-            imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+            RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP
+                    RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         }
 
         @Override
@@ -140,8 +139,8 @@ public class StarterBotTeleop extends OpMode {
             limelight.start();
 
             imu = hardwareMap.get(IMU.class,"imu");
-            RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                    RevHubOrientationOnRobot.UsbFacingDirection.DOWN);
+            RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                    RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
             imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
         }
 
