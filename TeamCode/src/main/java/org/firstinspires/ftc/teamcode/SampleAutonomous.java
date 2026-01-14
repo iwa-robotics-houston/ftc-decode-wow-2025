8c019d125c132243484372af88fab273fb83722d
Addy <addythescout@gmail.com>
2026-01-14 15:54:05 -0600

    updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutonomous.java
@@ -39,15 +39,11 @@ public class SampleAutonomous extends LinearOpMode
         {
             // Note, this example takes more than 30 seconds to execute, so turn OFF the auto timer.
 
-            // Drive a large rectangle, turning at each corner
-            robot.drive(  -5, 0.10, 0.05);
-            robot.turnTo(90, 0.05, 0.25);
-            robot.drive(  -5, 0.10, 0.05);
-            robot.turnTo(180, 0.05, 0.25);
-            robot.drive(  -5, 0.10, 0.05);
-            robot.turnTo(270, 0.05, 0.25);
-            robot.drive(  -5, 0.10, 0.05);
-            robot.turnTo(0, 0.05, 0.25);
+            // Drive the path again without turning.
+            robot.drive(  84, 0.60, 0.15);
+            robot.strafe( 72, 0.60, 0.15);
+            robot.drive( -84, 0.60, 0.15);
+            robot.strafe(-72, 0.60, 0.15);
 
             //sleep(500);
         }
