bd851db2fb1830946a86da9d0bc02a793ba38fc2
smichael28 <sophiemichael211@gmail.com>
2026-03-26 17:38:11 -0500

    Limelight added to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotTeleop.java
@@ -38,6 +38,8 @@ includes strafe and I'm hoping this will fix our driving and intake problems.
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 //import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -73,6 +75,8 @@ public class StarterBotTeleop extends OpMode {
 
     ElapsedTime runtime = new ElapsedTime();
 
+    private Limelight3A limelight;
+
     // This declares the motors + servos needed
     DcMotorEx frontLeftDrive;
     DcMotorEx frontRightDrive;
@@ -107,6 +111,12 @@ public class StarterBotTeleop extends OpMode {
         flywheelR = hardwareMap.get(DcMotorEx.class, "flywheelR");
         imu = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
         light = hardwareMap.get(RevBlinkinLedDriver.class, "light");
+
+        //init limelight
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+        //Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+        limelight.start();
 
 
         //  diverter = hardwareMap.get(Servo.class,"diverter");
@@ -167,6 +177,28 @@ public class StarterBotTeleop extends OpMode {
         double lateral = gamepad1.right_stick_x;
         double yaw = gamepad1.left_stick_x;
 
+//limelight loop
+        LLStatus status = limelight.getStatus();
+        telemetry.addData("Name", "%s",
+                status.getName());
+        telemetry.addData("LL", "Temp: %.1fC, CPU: %.1f%%, FPS: %d",
+                status.getTemp(), status.getCpu(),(int)status.getFps());
+        telemetry.addData("Pipeline", "Index: %d, Type: %s",
+                status.getPipelineIndex(), status.getPipelineType());
+
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+        telemetry.update();
 
         drive(axial, lateral, yaw);
 
